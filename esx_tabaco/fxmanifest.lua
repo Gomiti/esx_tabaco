cbb2a2854dd984ea8c94e723dcda1c3a4552acde
Gomiti <97442231+Gomiti@users.noreply.github.com>
2022-01-10 11:48:56 +0100

    Add files via upload

---
+++ esx_tabaco/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Gomiti'
+description 'esx_tabaco'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client/client.lua'
+}
+server_script {
+    'server/server.lua'
+}
